4634dff9795e49beb076b96d3256a28eacb01e4b
hamat <0922takm@gmail.com>
2026-04-26 17:37:47 +0900

    ブログ おすすめの記事を表示しよう

--- sidebar.php
+++ sidebar.php
@@ -1,68 +1,77 @@
-      <aside class="sidebar">
-        <div class="sidebar_mail">
-          <p class="sidebar_title">無料メールマガジン</p>
-          <div class="banner">
-            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/blog_details/banner.jpg" alt="バナー広告" /></a>
-          </div>
+<aside class="sidebar">
+  <div class="sidebar_mail">
+    <p class="sidebar_title">無料メールマガジン</p>
+    <div class="banner">
+      <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/blog_details/banner.jpg" alt="バナー広告" /></a>
+    </div>
+  </div>
+  <div class="sidebar_search">
+    <p class="sidebar_title">ブログ内を検索</p>
+    <div class="search_box">
+      <form action="search.html" method="GET" class="search-container">
+        <input type="search" name="q" class="search-input" placeholder="検索ワード" />
+        <div class="search_icon">
+          <button type="submit" class="search-button"></button>
         </div>
-        <div class="sidebar_search">
-          <p class="sidebar_title">ブログ内を検索</p>
-          <div class="search_box">
-            <form action="search.html" method="GET" class="search-container">
-              <input type="search" name="q" class="search-input" placeholder="検索ワード" />
-              <div class="search_icon">
-                <button type="submit" class="search-button"></button>
+      </form>
+    </div>
+  </div>
+  <div class="sidebar_posts">
+    <p class="sidebar_title">おすすめの記事</p>
+    <ul class="post_list">
+      <?php
+      $args = array(
+        'posts_per_page' => 3,
+        'post_type' => 'blog',
+        'taxonomy' => 'blog_recommend',
+        'term' => 'recommend',
+        'orderby' => 'date',
+        'order' => 'DESC'
+      );
+      $the_query = new WP_Query($args);
+      if ($the_query->have_posts()):
+        while ($the_query->have_posts()):
+          $the_query->the_post();
+          ?>
+          <li class="post_item">
+            <a href="<?php the_permalink(); ?>" class="post_link">
+              <div class="post_img_wrapper">
+                <?php if (has_post_thumbnail()): ?>
+                  <?php the_post_thumbnail(); ?>
+                <?php else: ?>
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/blog_details/side_img.jpg" alt="タイトル" />
+                <?php endif; ?>
               </div>
-            </form>
-          </div>
-        </div>
-        <div class="sidebar_posts">
-          <p class="sidebar_title">おすすめの記事</p>
-          <ul class="post_list">
-            <li class="post_item">
-              <a href="blog_details.html" class="post_link">
-                <div class="post_img_wrapper">
-                  <img src="<?php echo get_template_directory_uri(); ?>/img/blog_details/side_img.jpg" alt="タイトル" />
-                </div>
-                <p class="post_item_title">タイトルが入ります。タイトル</p>
-              </a>
-            </li>
-            <li class="post_item">
-              <a href="blog_details.html" class="post_link">
-                <div class="post_img_wrapper">
-                  <img src="<?php echo get_template_directory_uri(); ?>/img/blog_details/side_img.jpg" alt="タイトル" />
-                </div>
-                <p class="post_item_title">タイトルが入ります。タイトル</p>
-              </a>
-            </li>
-            <li class="post_item">
-              <a href="blog_details.html" class="post_link">
-                <div class="post_img_wrapper">
-                  <img src="<?php echo get_template_directory_uri(); ?>/img/blog_details/side_img.jpg" alt="タイトル" />
-                </div>
-                <p class="post_item_title">タイトルが入ります。タイトル</p>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="sidebar_category">
-          <p class="sidebar_title">カテゴリー</p>
-          <ul class="category_list">
-            <li class="category_item">
-              <a href="#" class="category_link">カテゴリー</a>
-            </li>
-            <li class="category_item">
-              <a href="#" class="category_link">カテゴリー</a>
-            </li>
-            <li class="category_item">
-              <a href="#" class="category_link">カテゴリー</a>
-            </li>
-            <li class="category_item">
-              <a href="#" class="category_link">カテゴリー</a>
-            </li>
-            <li class="category_item">
-              <a href="#" class="category_link">カテゴリー</a>
-            </li>
-          </ul>
-        </div>
-      </aside>
+              <p class="post_item_title">
+                <?php echo wp_trim_words(get_the_title(), 15, '...'); ?>
+              </p>
+            </a>
+            <?php
+        endwhile;
+        wp_reset_postdata();
+      endif;
+      ?>
+      </li>
+    </ul>
+  </div>
+  <div class="sidebar_category">
+    <p class="sidebar_title">カテゴリー</p>
+    <ul class="category_list">
+      <li class="category_item">
+        <a href="#" class="category_link">カテゴリー</a>
+      </li>
+      <li class="category_item">
+        <a href="#" class="category_link">カテゴリー</a>
+      </li>
+      <li class="category_item">
+        <a href="#" class="category_link">カテゴリー</a>
+      </li>
+      <li class="category_item">
+        <a href="#" class="category_link">カテゴリー</a>
+      </li>
+      <li class="category_item">
+        <a href="#" class="category_link">カテゴリー</a>
+      </li>
+    </ul>
+  </div>
+</aside>
