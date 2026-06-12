68f06aac1c5e702914f0d7e03ef63a13fcccfa11
hamat <0922takm@gmail.com>
2026-06-12 16:00:35 +0900

    SP版のホバー修正

--- functions.php
+++ functions.php
@@ -4,23 +4,23 @@
 // --------------------------------------------------
 function custom_theme_setup()
 {
-  add_theme_support('title-tag');
-  add_theme_support('automatic-feed-links');
-  add_theme_support('post-thumbnails');
-  add_theme_support(
-    'html5',
-    array(
-      'search-form',
-      'comment-form',
-      'comment-list',
-      'gallery',
-      'caption',
-      'style',
-      'script'
-    )
-  );
-  add_theme_support('wp-block-styles');
-  add_theme_support('responsive-embeds');
+    add_theme_support('title-tag');
+    add_theme_support('automatic-feed-links');
+    add_theme_support('post-thumbnails');
+    add_theme_support(
+        'html5',
+        array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'style',
+            'script'
+        )
+    );
+    add_theme_support('wp-block-styles');
+    add_theme_support('responsive-embeds');
 }
 add_action('after_setup_theme', 'custom_theme_setup');
 
@@ -29,57 +29,57 @@ add_action('after_setup_theme', 'custom_theme_setup');
 // --------------------------------------------------
 function add_files()
 {
-  $now = date('YmdHis');
+    $now = date('YmdHis');
 
-  // css登録
-  wp_register_style('common-style', get_theme_file_uri('/css/style.css'), array(), $now);
+    // css登録
+    wp_register_style('common-style', get_theme_file_uri('/css/style.css'), array(), $now);
 
-  // 共通CSS
-  wp_enqueue_style('slick-style', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), NULL);
-  wp_enqueue_style('common-style');
+    // 共通CSS
+    wp_enqueue_style('slick-style', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), NULL);
+    wp_enqueue_style('common-style');
 
-  // WordPress提供のjquery.jsを読み込まない
-  wp_deregister_script('jquery');
+    // WordPress提供のjquery.jsを読み込まない
+    wp_deregister_script('jquery');
 
-  // jQueryの読み込み
-  wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.7.1.min.js', "", NULL, false);
+    // jQueryの読み込み
+    wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.7.1.min.js', "", NULL, false);
 
-  //JS登録
-  wp_register_script('common-script', get_theme_file_uri('/js/script.js'), array('jquery'), $now, true);
+    //JS登録
+    wp_register_script('common-script', get_theme_file_uri('/js/script.js'), array('jquery'), $now, true);
 
-  // 共通のJS
-  wp_enqueue_script('slick-script', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), NULL, true);
-  wp_enqueue_script('common-script');
+    // 共通のJS
+    wp_enqueue_script('slick-script', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), NULL, true);
+    wp_enqueue_script('common-script');
 
-  if (is_front_page()) {
-    wp_enqueue_script('top-script', get_theme_file_uri('/js/top.js'), array('jquery'), $now, true);
-  }
+    if (is_front_page()) {
+        wp_enqueue_script('top-script', get_theme_file_uri('/js/top.js'), array('jquery'), $now, true);
+    }
 }
 add_action('wp_enqueue_scripts', 'add_files');
 
 function my_page_conditions($query)
 {
-  // 管理画面ではなく、メインクエリの場合のみ実行
-  if (!is_admin() && $query->is_main_query()) {
+    // 管理画面ではなく、メインクエリの場合のみ実行
+    if (!is_admin() && $query->is_main_query()) {
 
-    // カスタム投稿タイプ 'blog' または 'result' のアーカイブページの場合
-    if (is_post_type_archive(['blog', 'result'])) {
-      $query->set('posts_per_page', 10);
-    }
+        // カスタム投稿タイプ 'blog' または 'result' のアーカイブページの場合
+        if (is_post_type_archive(['blog', 'result'])) {
+            $query->set('posts_per_page', 10);
+        }
 
-    // 検索結果ページの場合
-    if ($query->is_search()) {
-      $query->set('post_type', 'blog');
+        // 検索結果ページの場合
+        if ($query->is_search()) {
+            $query->set('post_type', 'blog');
+        }
     }
-  }
 }
 add_action('pre_get_posts', 'my_page_conditions');
 
 //管理画面で 投稿メニュー を非表示
 function remove_menus()
 {
-  global $menu;
-  remove_menu_page('edit.php');
+    global $menu;
+    remove_menu_page('edit.php');
 }
 add_action('admin_menu', 'remove_menus');
 
@@ -90,44 +90,45 @@ add_filter('wpcf7_autop_or_not', '__return_false');
 //管理画面「外観＞メニュー」 を表示
 function register_my_theme_menus()
 {
-  register_nav_menus(array(
-    'primary' => 'PC用ナビゲーション',
-    'sp-menu' => 'スマホ用ナビゲーション',
-    'footer'  => 'Footer Menu',
-  ));
+    register_nav_menus(array(
+        'primary' => 'PC用ナビゲーション',
+        'sp-menu' => 'スマホ用ナビゲーション',
+        'footer' => 'Footer Menu',
+    ));
 }
 add_action('after_setup_theme', 'register_my_theme_menus');
 
 add_action('wp_enqueue_scripts', function () {
-  // フォームのID
-  $form_id = 'snow-monkey-form-269';
+    // フォームのID
+    $form_id = 'snow-monkey-form-269';
 
-  // 💡 リダイレクト先のURLを「/contact-send/」に設定しました！
-  $redirect_url = home_url('/contact-send/');
+    // 💡 リダイレクト先のURLを「/contact-send/」に設定しました！
+    $redirect_url = home_url('/contact-send/');
 
-  // JavaScriptをフッターに出力
-  ob_start();
-  ?>
-  <script>
-    document.addEventListener('DOMContentLoaded', function () {
-      var form = document.getElementById('<?php echo esc_js($form_id); ?>');
-      if (form) {
-        form.addEventListener('smf.submit', function (event) {
-          // 送信が正常に完了した（エラーがない）場合
-          if (event.detail.status === 'complete') {
-            window.location.href = '<?php echo esc_url($redirect_url); ?>';
-          }
+    // JavaScriptをフッターに出力
+    ob_start();
+    ?>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var form = document.getElementById('<?php echo esc_js($form_id); ?>');
+            if (form) {
+                form.addEventListener('smf.submit', function (event) {
+                    // 送信が正常に完了した（エラーがない）場合
+                    if (event.detail.status === 'complete') {
+                        window.location.href = '<?php echo esc_url($redirect_url); ?>';
+                    }
+                });
+            }
         });
-      }
-    });
-  </script>
-  <?php
-  $script = ob_get_clean();
-  wp_add_inline_script('snow-monkey-forms', strip_tags($script));
+    </script>
+    <?php
+    $script = ob_get_clean();
+    wp_add_inline_script('snow-monkey-forms', strip_tags($script));
 });
 
-function add_additional_class_on_li($classes, $item, $args) {
-    if(isset($args->add_li_class)) {
+function add_additional_class_on_li($classes, $item, $args)
+{
+    if (isset($args->add_li_class)) {
         $classes[] = $args->add_li_class;
     }
     return $classes;
@@ -206,11 +207,11 @@ function custom_meta_description(): void
     if (is_front_page()) {
         $description = '「音楽で生きる」を叶える ミュージックスクール「きたむらミュージックスクール」の公式ホームページです。';
 
-    // 固定ページ
+        // 固定ページ
     } elseif (is_page()) {
         $description = 'きたむらミュージックスクール公式ホームページの' . get_the_title() . 'ページです。';
 
-    // 投稿個別ページ
+        // 投稿個別ページ
     } elseif (is_single()) {
         if (has_excerpt()) {
             $description = get_the_excerpt();
@@ -221,7 +222,7 @@ function custom_meta_description(): void
             $description = mb_substr($content, 0, 120, 'UTF-8');
         }
 
-    // 投稿アーカイブ
+        // 投稿アーカイブ
     } elseif (is_archive()) {
         if (is_category()) {
             $name = single_cat_title('', false);
@@ -235,15 +236,15 @@ function custom_meta_description(): void
 
         $description = 'きたむらミュージックスクール公式ホームページの' . $name . '一覧ページです。';
 
-    // 検索結果
+        // 検索結果
     } elseif (is_search()) {
         $description = 'きたむらミュージックスクール公式ホームページの検索結果ページです。';
 
-    // 404
+        // 404
     } elseif (is_404()) {
         $description = 'きたむらミュージックスクール公式ホームページの404ページです。';
 
-    // その他のページ
+        // その他のページ
     } else {
         $description = 'きたむらミュージックスクール公式ホームページの' . get_the_title() . 'ページです。';
     }
