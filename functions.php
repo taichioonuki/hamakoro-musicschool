2bc221f5e5fbf7a894a04322414a138e098bff40
hamat <0922takm@gmail.com>
2026-06-17 10:50:07 +0900

    タイムスタンプの修正

--- functions.php
+++ functions.php
@@ -31,24 +31,21 @@ function add_files()
 {
     $now = date('YmdHis');
 
-    // css登録
-    wp_register_style('common-style', get_theme_file_uri('/css/style.css'), array(), $now);
-
-    // 共通CSS
-    wp_enqueue_style('slick-style', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), NULL);
-    wp_enqueue_style('common-style');
+    // 💡【修正完了】cssの登録と読み込みを、直接タイムスタンプ（$now）付きで実行
+    wp_enqueue_style('slick-style', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), null);
+    wp_enqueue_style('common-style', get_theme_file_uri('/css/style.css'), array(), $now);
 
     // WordPress提供のjquery.jsを読み込まない
     wp_deregister_script('jquery');
 
     // jQueryの読み込み
-    wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.7.1.min.js', "", NULL, false);
+    wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.7.1.min.js', array(), null, false);
 
-    //JS登録
+    // JS登録
     wp_register_script('common-script', get_theme_file_uri('/js/script.js'), array('jquery'), $now, true);
 
     // 共通のJS
-    wp_enqueue_script('slick-script', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), NULL, true);
+    wp_enqueue_script('slick-script', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), null, true);
     wp_enqueue_script('common-script');
 
     if (is_front_page()) {
@@ -261,8 +258,8 @@ add_action('wp_head', 'custom_meta_description', 1);
 
 add_filter(
     'snow_monkey_forms/validator/error_message',
-    function( $message, $validation_name, $name ) {
-        if ( 'required' === $validation_name ) {
+    function ($message, $validation_name, $name) {
+        if ('required' === $validation_name) {
             return 'この項目は必須です。';
         }
         return $message;
